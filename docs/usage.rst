7b8be38491970266f776d1fbaf303107e6355847
Christopher Doyle <christophercormac@outlook.com>
2020-02-03 13:45:11 +0000

    Initial commit

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use PySpy in a project::
+
+    import pyspy
---
+++ pyspy/__init__.py
@@ -0,0 +1,4 @@
+"""Top-level package for PySpy."""
+
+__author__ = """Christopher Doyle"""
+__version__ = "0.1.0"
