964025d98a642d767ecdaa99488925773cf1a169
Christopher Doyle <christophercormac@outlook.com>
2020-02-03 14:20:15 +0000

    Refactored for reusability

--- tests/test_pyspy.py
+++ tests/test_pyspy.py
@@ -1,7 +1,23 @@
 import sys
 from queue import Queue
 
+import pytest
+
 from pyspy import pyspy
+
+
+@pytest.fixture
+def fake_module():
+    class FakeTestingModule:
+        @staticmethod
+        def my_fun():
+            pass
+
+    name = FakeTestingModule.__name__
+    assert name not in sys.modules
+    sys.modules[name] = FakeTestingModule
+    yield FakeTestingModule
+    del sys.modules[name]
 
 
 class BaseTest:
@@ -9,16 +25,11 @@ class BaseTest:
 
 
 class TestWiretapFunction(BaseTest):
-    def test_logbook_contains_entry_after_calling_wiretapped_function(self):
-        class FakeTestingModule:
-            @staticmethod
-            def my_fun():
-                return
-
-        sys.modules["FakeTestingModule"] = FakeTestingModule
-
+    def test_logbook_contains_entry_after_calling_wiretapped_function(
+        self, fake_module
+    ):
         logbook = Queue()
-        pyspy.wiretap_function(sys.modules["FakeTestingModule"], "my_fun", logbook)
+        pyspy.wiretap_function(fake_module, "my_fun", logbook)
         assert logbook.empty()
-        FakeTestingModule.my_fun()
+        fake_module.my_fun()
         assert not logbook.empty()
